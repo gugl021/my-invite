00db7d5db6e6075fc584538b782287737b4dca12
gugl021 <gugl021@gmail.com>
2024-07-05 13:56:44 +0200

    Added data

---
+++ api/rsvp.js
@@ -0,0 +1,27 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+export default async function handler(req, res) {
+  // eslint-disable-next-line no-undef
+  const filePath = path.join(process.cwd(), "data.json");
+
+  if (req.method === "GET") {
+    try {
+      const fileContents = await fs.readFile(filePath, "utf-8");
+      const data = JSON.parse(fileContents);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to read data from file" });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const data = req.body;
+      await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+      res.status(200).json({ message: "Data written to file" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to write data to file" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
